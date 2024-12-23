e2975d0c8717620d885e61c701edcd0a50929d70
maketea90 <joerollerson@gmail.com>
2024-12-23 04:46:35 +0000

    add access control allow origin *

--- app.js
+++ app.js
@@ -25,6 +25,11 @@ var app = express();
 
 app.use(cors())
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
